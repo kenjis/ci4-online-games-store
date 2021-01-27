905837e703330a6d54f0fc914820caa9963b569d
kenjis <kenji.uui@gmail.com>
2021-01-27 15:48:33 +0900

    migrate: copy controllers

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,13 +1,31 @@
-<?php
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-declare(strict_types=1);
+class Home extends CI_Controller {
+	
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('home_model', 'home');
+	}
+	
+	public function index()
+	{
+		$data['title'] 	= 'Home';
+		$data['games']		= $this->home->getAllGame();
+		$data['banners']	= $this->home->getAllBanner();
+		$data['page']		= 'pages/home/index';
+		$this->load->view('layouts/app', $data);
+	}
 
-namespace App\Controllers;
+	public function detail($id)
+	{
+		$data['title'] = 'Detail Game';
+		$data['game']	= $this->home->getGameById($id);
+		$data['page']	= 'pages/home/detail';
+		$this->load->view('layouts/app', $data);
+	}
 
-class Home extends BaseController
-{
-    public function index(): string
-    {
-        return view('welcome_message');
-    }
 }
+
+/* End of file Home.php */
